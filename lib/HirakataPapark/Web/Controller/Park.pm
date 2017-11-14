691781f1bfc63d6a7fedd859356e3565de1e4f08
liiu <yliiu_nan-na@docomo.ne.jp>
2017-11-14 23:40:41 +0900

    fix W::C::Park

--- lib/HirakataPapark/Web/Controller/Park.pm
+++ lib/HirakataPapark/Web/Controller/Park.pm
@@ -2,28 +2,51 @@ package HirakataPapark::Web::Controller::Park {
 
   use Mojo::Base 'HirakataPapark::Web::Controller';
   use HirakataPapark;
-  use HirakataPapark::Service::Park;
 
-  has 'service' => sub { HirakataPapark::Service::Park->new };
+  use HirakataPapark::Model::Parks;
+  use HirakataPapark::Model::Parks::Plants;
+  use HirakataPapark::Model::Parks::Comments;
+
+  use constant DEFAULT_COMMENT_NUM => 5;
+
+  has 'park_id' => sub {
+    my $self = shift;
+    $self->param('park_id');
+  };
+  has 'parks'         => sub { HirakataPapark::Model::Parks->new };
+  has 'park_plants'   => sub { HirakataPapark::Model::Parks::Plants->new };
+  has 'park_comments' => sub { HirakataPapark::Model::Parks::Comments->new };
 
   sub show_park_by_id {
     my $self = shift;
-    my $result = $self->service->get_park_by_id( $self->param('park_id') );
-    $self->stash($result);
-    $self->render_to_multiple_lang();
+    $self->parks->get_row_by_id($self->park_id)->match(
+      Some => sub {
+        my $park = shift;
+        $self->stash(park => $park);
+        $self->render_to_multiple_lang();
+      },
+      None => sub { $self->reply->not_found() },
+    );
   }
 
   sub show_park_plants_by_id {
     my $self = shift;
-    my $result = $self->service->get_park_plants_by_id( $self->param('park_id') );
-    $self->stash($result);
+    my $plants = [
+      sort { $a->category cmp $b->category }
+      $self->park_plants->get_rows_by_park_id($self->park_id)->@*
+    ];
+    my $park = $self->parks->get_row_by_id($self->park_id)->get;
+    $self->stash({
+      park   => $park,
+      plants => $plants,
+    });
     $self->render_to_multiple_lang();
   }
 
   sub add_comment_by_id {
     my $self = shift;
-    $self->service->add_comment_by_id({
-      park_id => $self->param('park_id'),
+    $self->park_comments->add_row({
+      park_id => $self->park_id,
       name    => $self->param('name') || '名無し',
       message => $self->param('message'),
     });
@@ -32,7 +55,8 @@ package HirakataPapark::Web::Controller::Park {
 
   sub get_comments_by_id {
     my $self = shift;
-    my $comments = $self->service->get_comments_by_id( $self->param('park_id') );
+    my $comments = $self->park_comments
+      ->get_rows_by_park_id($self->park_id, DEFAULT_COMMENT_NUM);
     $self->render(comments => $comments);
   }
 
